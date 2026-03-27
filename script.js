f80d84ec048e885298839ce01b12ef5142da5d57
Akash <akash629554@gmail.com>
2026-03-27 12:41:41 +0530

    Refactor link shortening to use custom GitHub page
    Updated the link shortening logic to use a custom GitHub page link instead of the TinyURL API.

--- script.js
+++ script.js
@@ -8,23 +8,12 @@ function shortenUrl() {
         return;
     }
 
-    resultDiv.innerHTML = "অপেক্ষা করুন...";
+    // আপনার অরিজিনাল লিংকটিকে লুকিয়ে ফেলা (Encode করা)
+    let encodedUrl = btoa(originalUrl);
 
-    // TinyURL API-এর মাধ্যমে লিংক ছোট করা
-    fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(originalUrl)}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response error');
-            }
-            return response.text();
-        })
-        .then(data => {
-            // সফল হলে ছোট লিংকটি দেখানো
-            resultDiv.innerHTML = `শর্ট লিংক: <a href="${data}" target="_blank">${data}</a>`;
-        })
-        .catch(error => {
-            // কোনো সমস্যা হলে
-            resultDiv.innerHTML = "<span style='color:red;'>কোথাও কোনো সমস্যা হয়েছে। লিংকটি সঠিক কিনা চেক করুন।</span>";
-            console.error('Error:', error);
-        });
+    // আপনার গিটহাব পেজের লিংক (এর সাথে page1.html যুক্ত করা হলো)
+    let myCustomLink = "https://akash629554-cyber.github.io/Website-/page1.html?link=" + encodedUrl;
+
+    // সফল হলে নতুন লিংকটি দেখানো
+    resultDiv.innerHTML = `আপনার নতুন লিংক: <br><br> <a href="${myCustomLink}" target="_blank" style="word-wrap: break-word;">${myCustomLink}</a>`;
 }
